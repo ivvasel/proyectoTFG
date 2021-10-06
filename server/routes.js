929a4ee1ca13ed565a08924ce793531256223291
ivvasel <ivvasel@teleco.upv.es>
2021-10-07 00:06:10 +0200

    Instalación server con Node

---
+++ server/routes.js
@@ -0,0 +1,18 @@
+//Route.js - Módulo de rutas
+var express = require('express');
+var app = express();
+
+
+// Get mensajes
+app.get('/', function (req, res) {
+    //res.json()
+
+})
+
+// Post mensajes
+app.post('/', function (req, res) {
+    //res.json()
+
+})
+
+module.exports = app;
