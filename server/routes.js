bbe7e6f3b2ab8903c833bba4b82426afce49020f
ivvasel <ivvasel@teleco.upv.es>
2021-10-17 20:20:55 +0200

    Conectamos con BD Firestore y registramos users

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,14 @@
 var {Router} = require('express');
 var app = Router();
 
+//Inicializamos Cloud Firestore Database
+const admin = require('firebase-admin');
+const serviceAccount = require('../service-account.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
 
 // Get mensajes
 app.get('/', function (req, res) {
@@ -14,12 +22,38 @@ app.get('/login', (req,res) =>{
 
     res.render('login', {titulo: "Login"});
 
+
 })
 
 // Post mensajes
 app.post('/', function (req, res) {
     //res.json()
+    res.render('index');
 
 })
+
+app.post('/login', (req,res) =>{
+
+    console.log(req.body);
+
+    const data = {
+        nick: req.body.nick,
+        nombre: req.body.nombre,
+        apellido: req.body.apellidos,   
+        mail: req.body.mail,
+        //peso: req.body.peso
+    };
+
+    const docRef = db.collection('users').doc(req.body.nick).set(data);;
+    
+
+
+
+
+
+    res.redirect('/'); //Redigir cuando se realiza el login
+
+});
+
 
 module.exports = app;
